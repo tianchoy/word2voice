b883d935b74aa295a6ed6af7e6ecca2d3ed5f7bc
tianchoy <tianchoy@live.com>
2023-11-16 09:39:22 +0800

    V1.2

--- SparkApi/test.py
+++ SparkApi/test.py
@@ -1,37 +1,38 @@
 from tkinter import *
 from tkinter.font import Font
 import SparkApi
-#以下密钥信息从控制台获取
-appid = ""     #填写控制台中获取的 APPID 信息
-api_secret = ""   #填写控制台中获取的 APISecret 信息
-api_key =""    #填写控制台中获取的 APIKey 信息
 
-#用于配置大模型版本，默认“general/generalv2”
-domain = "general"   # v1.5版本
+# 以下密钥信息从控制台获取
+appid = ""  # 填写控制台中获取的 APPID 信息
+api_secret = ""  # 填写控制台中获取的 APISecret 信息
+api_key = ""  # 填写控制台中获取的 APIKey 信息
+
+# 用于配置大模型版本，默认“general/generalv2”
+domain = "general"  # v1.5版本
 # domain = "generalv2"    # v2.0版本
-#云端环境的服务地址
+# domain = "generalv3"   #v3.0版本
+# 云端环境的服务地址
 Spark_url = "ws://spark-api.xf-yun.com/v1.1/chat"  # v1.5环境的地址
 # Spark_url = "ws://spark-api.xf-yun.com/v2.1/chat"  # v2.0环境的地址
-
+# Spark_url = "ws://spark-api.xf-yun.com/v3.1/chat" #v3.0环境地址
 modelType = '1.5'
 
 win = Tk()
-win.title("讯飞星火V1.0")
-win.geometry("700x500")
+win.title("讯飞星火V1.2")
+win.geometry("750x550")
 
 scrollbar = Scrollbar(win)
 scrollbar.pack(side=RIGHT, fill=Y)
 
-font = Font(family="Microsoft YaHei", size=10)
-
-text =[]
-
-def getText(role,content):
+font = Font(family="Driod Sans Mono", size=10)  # 设置显示字体
+text = []
+def getText(role, content):
     jsoncon = {}
     jsoncon["role"] = role
     jsoncon["content"] = content
     text.append(jsoncon)
     return text
+
 
 def getlength(text):
     length = 0
@@ -41,12 +42,15 @@ def getlength(text):
         length += leng
     return length
 
+
 def checklen(text):
-    while (getlength(text) > 8000):
+    while getlength(text) > 8000:
         del text[0]
     return text
 
+
 def print_selection():
+    print(var.get())
     global domain
     global Spark_url
     global modelType
@@ -54,11 +58,14 @@ def print_selection():
         domain = "general"
         Spark_url = "ws://spark-api.xf-yun.com/v1.1/chat"
         modelType = '1.5'
-    else:
+    elif var.get() == 2:
         domain = "generalv2"
         Spark_url = "ws://spark-api.xf-yun.com/v2.1/chat"
         modelType = '2.0'
-
+    else :
+        domain = "generalv3"
+        Spark_url = "ws://spark-api.xf-yun.com/v3.1/chat"
+        modelType = '3.0'
 
 
 # 运行函数
@@ -66,34 +73,46 @@ def runbuild():
     Input = str(inp1.get())
     inputText = ("我：" + Input + '\n')
     texts.insert(END, inputText)
-    question = checklen(getText("user",Input))
+    question = checklen(getText("user", Input))
     SparkApi.answer = ""
-    SparkApi.main(appid,api_key,api_secret,Spark_url,domain,question)
-    res = ('星火('+modelType+')回答：'+SparkApi.answer+'\n\n')
-    texts.insert(END,str(res))
+    SparkApi.main(appid, api_key, api_secret, Spark_url, domain, question)
+    res = ('星火(' + modelType + ')回答：' + SparkApi.answer + '\n\n')
+    texts.insert(END, str(res))
+
+# 清空输入
+def clearbuild():
+    inp1.delete(0, END)  # 清空输入
+    texts.delete(1.0, END)
 
 
 # 布局窗体
 var = IntVar()
-var.set(1)
+var.set(1) # 设置默认选择1.5模型
 
 r1 = Radiobutton(win, text="1.5模型", variable=var, value=1, command=print_selection)
 r1.pack()
-r1.place(x=10, y=5, width=100, height=20)
+r1.place(x=15, y=55, width=70, height=20)
 
 r2 = Radiobutton(win, text="2.0模型", variable=var, value=2, command=print_selection)
 r2.pack()
-r2.place(x=10, y=25, width=100, height=20)
+r2.place(x=80, y=55, width=70, height=20)
 
-inp1 = Entry(win, textvariable=StringVar(value='请输入问题'),font=font)
-inp1.place(relx=0.15, rely=0.03, relwidth=0.7, relheight=0.05)
+r3 = Radiobutton(win, text="3.0模型(New)", variable=var, value=3, command=print_selection, state='disabled')
+r3.pack()
+r3.place(x=150, y=55, width=100, height=20)
 
-texts = Text(win, wrap=WORD,yscrollcommand=scrollbar.set,font=font,padx=10, pady=10)
-texts.place(relx=0.03, rely=0.1, relwidth=0.94, relheight=0.85)
+inp1 = Entry(win, textvariable=StringVar(value='请输入问题'), font=font)
+inp1.place(relx=0.03, rely=0.03, relwidth=0.7, relheight=0.05)
+
+texts = Text(win, wrap=WORD, yscrollcommand=scrollbar.set, font=font, padx=10, pady=10)
+texts.place(relx=0.03, rely=0.15, relwidth=0.94, relheight=0.82)
 scrollbar.config(command=texts.yview)
 
-btn = Button(win, text="生成结果",command=runbuild)
-btn.place(relx=0.87, rely=0.03, relwidth=0.1, relheight=0.05)
+btn = Button(win, text="生成结果", command=runbuild)
+btn.place(relx=0.77, rely=0.03, relwidth=0.1, relheight=0.05)
+
+btn1 = Button(win, text="清除", command=clearbuild)
+btn1.place(relx=0.88, rely=0.03, relwidth=0.08, relheight=0.05)
 
 # 载入窗体
 win.mainloop()
