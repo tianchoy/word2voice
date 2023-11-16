6812120961af16d13a80997093ac5e7c2b06d7f5
tianchoy <tianchoy@live.com>
2023-11-16 14:37:17 +0800

    V1.3

--- SparkApi/test.py
+++ SparkApi/test.py
@@ -4,6 +4,7 @@ import SparkApi
 import threading
 import pyperclip
 from tkinter import messagebox
+import pyttsx3
 
 # 以下密钥信息从控制台获取
 appid = ""  # 填写控制台中获取的 APPID 信息
@@ -19,13 +20,26 @@ Spark_url = "ws://spark-api.xf-yun.com/v1.1/chat"  # v1.5环境的地址
 # Spark_url = "ws://spark-api.xf-yun.com/v2.1/chat"  # v2.0环境的地址
 # Spark_url = "ws://spark-api.xf-yun.com/v3.1/chat" #v3.0环境地址
 modelType = '1.5'
-winTitle = '讯飞星火V1.2'
+winTitle = '讯飞星火V1.3'
 winSize = "750x550"
+answerContent = ''
 
+# 初始化窗体
 win = Tk()
 win.title(winTitle)
 win.geometry(winSize)
 
+
+# 初始化语音插件
+engine = pyttsx3.init()
+engine.setProperty('rate',180)
+engine.setProperty('volume',1)
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[0].id)
+
+
+# 滚动条设置
 scrollbar = Scrollbar(win)
 scrollbar.pack(side=RIGHT, fill=Y)
 
@@ -77,33 +91,29 @@ def print_selection():
 
 
 def fetch_data():
-    try:
-        Input = str(inp1.get())
-        inputText = ("我：" + Input + '\n')
-        texts.insert(END, inputText)
-        question = checklen(getText("user", Input))
-        SparkApi.answer = ""
-        SparkApi.main(appid, api_key, api_secret, Spark_url, domain, question)
-        res = ('星火(' + modelType + ')回答：' + SparkApi.answer + '\n\n')
-        texts.insert(END, str(res))
-        btn.config(text='生成结果', state='normal')
-    except:
-        messagebox.showinfo(winTitle, "生成失败")
+    global answerContent
+    Input = str(inp1.get())
+    inputText = ("我：" + Input + '\n')
+    texts.insert(END, inputText)
+    question = checklen(getText("user", Input))
+    SparkApi.answer = ""
+    SparkApi.main(appid, api_key, api_secret, Spark_url, domain, question)
+    res = ('星火(' + modelType + ')回答：' + SparkApi.answer + '\n\n')
+    answerContent = SparkApi.answer
+    texts.insert(END, str(res))
+    btn.config(text='生成', state='normal')
 
 # 运行函数
 def runbuild():
     thread = threading.Thread(target=fetch_data)
     thread.start()
-    btn.config(text='生成中……', state='disabled')
+    btn.config(text='生成中…', state='disabled')
 
 
 # 清空输入
 def clearbuild():
-    try:
-        inp1.delete(0, END)  # 清空输入
-        texts.delete(1.0, END)
-    except:
-        messagebox.showinfo(winTitle, "清空失败")
+    inp1.delete(0, END)  # 清空输入
+    texts.delete(1.0, END)
 
 
 def copyContent():
@@ -111,12 +121,24 @@ def copyContent():
     if content != '':
         try:
             pyperclip.copy(content)
-            messagebox.showinfo(winTitle, "复制成功！")
+            messagebox.showinfo(winTitle, "复制成功")
         except:
-            messagebox.showinfo(winTitle, "无法复制！")
+            messagebox.showinfo(winTitle, "无法复制")
     else:
-        messagebox.showinfo(winTitle, "复制内容为空！")
+        messagebox.showinfo(winTitle, "空内容复制个啥？")
 
+# 阅读线程
+def readThread():
+    engine.say(answerContent)
+    engine.runAndWait()
+    engine.stop()
+    btn3.config(text='朗读', state='normal')
+
+# 阅读内容
+def readContent():
+    thread = threading.Thread(target=readThread)
+    thread.start()
+    btn3.config(text='朗读中…', state='disabled')
 
 # 布局窗体
 var = IntVar()
@@ -142,13 +164,16 @@ texts.place(relx=0.03, rely=0.15, relwidth=0.94, relheight=0.82)
 scrollbar.config(command=texts.yview)
 
 btn = Button(win, text="生成", command=runbuild)
-btn.place(relx=0.75, rely=0.03, relwidth=0.1, relheight=0.05)
+btn.place(relx=0.73, rely=0.03, relwidth=0.07, relheight=0.05)
 
 btn1 = Button(win, text="清除", command=clearbuild)
-btn1.place(relx=0.86, rely=0.03, relwidth=0.05, relheight=0.05)
+btn1.place(relx=0.8, rely=0.03, relwidth=0.05, relheight=0.05)
 
-btn1 = Button(win, text="复制", command=copyContent)
-btn1.place(relx=0.92, rely=0.03, relwidth=0.05, relheight=0.05)
+btn2 = Button(win, text="复制", command=copyContent)
+btn2.place(relx=0.85, rely=0.03, relwidth=0.05, relheight=0.05)
+
+btn3 = Button(win, text="朗读", command=readContent)
+btn3.place(relx=0.9, rely=0.03, relwidth=0.07, relheight=0.05)
 
 # 载入窗体
 win.mainloop()
